b1dc5769b332bccc5c95988f56c9cab79525bae8
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2020-11-15 14:53:17 -0800

    -Set up routes and installed Semantic UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import Home from "./views/Home";
+import Register from "./views/Register";
+import Login from "./views/Login";
 
 const App = () => {
-  return <div>Greetings</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
