5a81db3092ae79d89b6ac1c300c4b492771f2bc1
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2020-11-20 14:57:39 -0800

    -Posts will now render without refresh -Working on delete without refresh

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -18,7 +18,12 @@ function PostForm() {
         query: FETCH_POSTS_QUERY,
       });
       data.getPosts = [result.data.createPost, ...data.getPosts];
-      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          getPosts: [result.data.createPost, ...data.getPosts],
+        },
+      });
       values.body = "";
     },
   });
